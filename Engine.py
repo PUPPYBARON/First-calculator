b8e00ff07fd8248622734d5b93b0aed7ed9ef84b
PUPPYBARON <86602339+PUPPYBARON@users.noreply.github.com>
2021-07-03 19:53:55 +1100

    update engine.py

--- Engine.py
+++ Engine.py
@@ -3,15 +3,15 @@ class Engine():
     def __init__(self):
         print("Engine has been started")
     
-    def compute(self,memory,a_sigh):
+    def compute(self,memory,memory2,a_sigh):
         if a_sigh == '+':
-            answer = memory + memory 
+            answer = memory + memory2 
         elif a_sigh == '-':
-            answer = memory - memory
+            answer = memory - memory2
         elif a_sigh == '/':
-            answer = memory / memory
+            answer = memory / memory2
         elif a_sigh == 'x':
-            answer = memory * memory
+            answer = memory * memory2
         else:
             print("ERROR")
         return answer 
